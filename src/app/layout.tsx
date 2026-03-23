a2b2ace6772cf7191ee853ed04f564e3b48849fd
valleywindowcare <james@valleywindowcare.com>
2026-03-22 21:31:00 -0500

    fix: eagerly load meta pixel asynchronously outside of interactions to track landing page attribution

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import MobileActionBar from "@/components/MobileActionBar";
 import MobileFooter from "@/components/MobileFooter";
 import MarketingScripts from "@/components/MarketingScripts";
+import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -90,6 +91,26 @@ export default function RootLayout({
         <SpeedInsights />
         <Analytics />
         <MarketingScripts />
+        {process.env.NEXT_PUBLIC_META_PIXEL_ID && (
+          <Script
+            id="fb-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                 !function(f,b,e,v,n,t,s)
+                 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                 n.queue=[];t=b.createElement(e);t.async=!0;
+                 t.src=v;s=b.getElementsByTagName(e)[0];
+                 s.parentNode.insertBefore(t,s)}(window, document,'script',
+                 'https://connect.facebook.net/en_US/fbevents.js');
+                 fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
+                 fbq('track', 'PageView');
+               `,
+            }}
+          />
+        )}
         <Header />
         <main className="flex-grow">
           {children}
