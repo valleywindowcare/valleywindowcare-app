f13aed3ae753e35e9169eec8f0a8dfd9ab5ff5c4
valleywindowcare <james@valleywindowcare.com>
2026-09-24 19:16:00 -0500

    feat(seo): configure explicit crawler permissions in app/robots.ts for Googlebot and Google-StoreBot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,35 +1,26 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: [
-            {
-                userAgent: [
-                    'Googlebot',
-                    'OAI-SearchBot',
-                    'ChatGPT-User',
-                    'PerplexityBot',
-                    'Claude-Web',
-                ],
-                allow: '/',
-                disallow: ['/api/', '/quote/success'],
-            },
-            {
-                userAgent: [
-                    'GPTBot',
-                    'ClaudeBot',
-                    'Amazonbot',
-                    'Bytespider',
-                    'CCBot',
-                ],
-                disallow: ['/'],
-            },
-            {
-                userAgent: '*',
-                allow: '/',
-                disallow: ['/api/', '/quote/success'],
-            },
-        ],
-        sitemap: 'https://valleyexteriorpros.com/sitemap.xml',
-    };
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Google-StoreBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot-Image',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://valleyexteriorpros.com/sitemap.xml',
+  };
 }
